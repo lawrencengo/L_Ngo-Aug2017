c9f35e6205ec8986c13abae29cc7b3a7f03e24d6
lawrencengo <18ngol18@millshighsch.org>
2017-08-22 09:28:28 -0700

    second project-- using Git from Eclipse

---
+++ project2/src/project2/project2.java
@@ -0,0 +1,13 @@
+package project2;
+// Lawrence Ngo
+// My second java project. It will compare pring and println.
+// August 22, 2017
+
+public class project2 {
+
+	public static void main(String[] args) {
+		System.out.println("hello-- project 2");
+		System.out.print("hello-- project 2");
+	}
+
+}
