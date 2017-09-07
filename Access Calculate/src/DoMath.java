9b2a500d809aaec4d2d762361c7c721e9740f3c5
lawrencengo <31105327+lawrencengo@users.noreply.github.com>
2017-09-07 08:24:37 -0700

    New

--- Access Calculate/src/DoMath.java
+++ Access Calculate/src/DoMath.java
@@ -8,7 +8,11 @@ public class DoMath {
 		System.out.println(Calculate.cube(5));
 		System.out.println(Calculate.average(5, 2));
 		System.out.println(Calculate.average(5,6,7));
-		System.out.println(Calculate.average();
+		System.out.println(Calculate.toDegrees(1.570795));
+		System.out.println(Calculate.toRadians(90));
+		System.out.println(Calculate.discriminant(3,4,5));
+		System.out.println(Calculate.toImproperFrac(3,1,2));
+		System.out.println(Calculate.toMixedNum(7, 2));
 	}
 
 }
--- Calculate Library/src/Calculate.java
+++ Calculate Library/src/Calculate.java
@@ -18,8 +18,32 @@ public class Calculate {
 		return ((operand + operand1) / 2);
 	}
 	
-	// This method accpets three doubles and returns a double.
+	// This method accepts three doubles and returns a double.
 	public static double average(double operand, double operand1, double operand2) {
 		return ((operand+operand1+operand2)/3);
+		
+	}
+	// This method accepts a double and returns a double. (radians->degrees)
+	public static double toDegrees(double operand) {
+		return (operand*180/3.14159);
+	}
+	// This method accepts a double and returns a double. (degrees->radians)
+	public static double toRadians(double operand) {
+		return (operand*3.14159/180);
+	}
+	// This method accepts three doubles and returns a double (provides coefficients of a quadratic equation in standard form (a,b,c) returns value of discriminant)
+	public static double discriminant(double a, double b, double c) {
+		return (b*b)-(4*a*c);
+	}
+	// This methods accepts three integers and returns a string.
+	public static String toImproperFrac(double a, double b, double c) {
+		return (((a*c)+ b)+ "/"+c);
+	}
+	// This method converts an improper fraction into a mixed number.
+	public static String toMixedNum(double a, double b) {
+		return null;
+		
 	}
 }
+
+
