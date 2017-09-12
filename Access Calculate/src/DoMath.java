52b5f2ac6e969e520742664188ee36d4fc47f658
lawrencengo <31105327+lawrencengo@users.noreply.github.com>
2017-09-11 20:37:08 -0700

    new

--- Access Calculate/src/DoMath.java
+++ Access Calculate/src/DoMath.java
@@ -1,6 +1,6 @@
 // Lawrence Ngo
 // September 5, 2017
-// This is the runner class for my Calculate Library methos.
+// This is the runner class for my Calculate Library methods.
 public class DoMath {
 
 	public static void main(String[] args) {
@@ -14,7 +14,12 @@ public class DoMath {
 		System.out.println(Calculate.toImproperFrac(3,1,2));
 		System.out.println(Calculate.toMixedNum(7, 2));
 		System.out.println(Calculate.foil(2,3,6,-7, null));
-		
+		System.out.println(Calculate.isDivisibleBy(4, 2));
+		System.out.println(Calculate.absValue(-2)); 
+		System.out.println(Calculate.max(3, 4));
+		System.out.println(Calculate.max(3,4,5));
+		System.out.println(Calculate.min(6,7));
+		System.out.println(Calculate.round2(2.678));
 	}
 
 }
--- Calculate Library/src/Calculate.java
+++ Calculate Library/src/Calculate.java
@@ -17,42 +17,106 @@ public class Calculate {
 	public static double average(double operand, double operand1) {
 		return ((operand + operand1) / 2);
 	}
-	
+
 	// This method accepts three doubles and returns a double.
 	public static double average(double operand, double operand1, double operand2) {
-		return ((operand+operand1+operand2)/3);
-		
+		return ((operand + operand1 + operand2) / 3);
+
 	}
+
 	// This method accepts a double and returns a double. (radians->degrees)
 	public static double toDegrees(double operand) {
-		return (operand*180/3.14159);
+		return (operand * 180 / 3.14159);
 	}
+
 	// This method accepts a double and returns a double. (degrees->radians)
 	public static double toRadians(double operand) {
-		return (operand*3.14159/180);
+		return (operand * 3.14159 / 180);
 	}
-	// This method accepts three doubles and returns a double (provides coefficients of a quadratic equation in standard form (a,b,c) returns value of discriminant)
+
+	// This method accepts three doubles and returns a double (provides coefficients
+	// of a quadratic equation in standard form (a,b,c) returns value of
+	// discriminant)
 	public static double discriminant(double a, double b, double c) {
-		return (b*b)-(4*a*c);
+		return (b * b) - (4 * a * c);
 	}
+
 	// This methods accepts three integers and returns a string.
 	public static String toImproperFrac(double a, double b, double c) {
-		return (((a*c)+ b)+ "/"+c);
+		return (((a * c) + b) + "/" + c);
 	}
+
 	// This method converts an improper fraction into a mixed number.
 	public static String toMixedNum(double a, double b) {
-		double wholeNum= a/b;
-		double newNumerator = a%b;
-		return (wholeNum+"_"+newNumerator+"/"+b);
+		double wholeNum = a / b;
+		double newNumerator = a % b;
+		return (wholeNum + "_" + newNumerator + "/" + b);
 	}
+
 	// This method converts a binomial multiplication into a quadratic equation.
 	public static String foil(int a, int b, int c, int d, String n) {
-		int first= (a*c);
-		int second=(a*d)+(b*c);
-		int third=(b*d);
-		return(first + "n^2"+ "+" +second+ "x"+"+"+third);
-		
+		int first = (a * c);
+		int second = (a * d) + (b * c);
+		int third = (b * d);
+		return (first + "n^2" + "+" + second + "x" + "+" + third);
+
+	}
+
+	// This method determines whether or not one integer is evenly divisible by
+	// another.
+	public static boolean isDivisibleBy(int num1, int num2) {
+		if (num1 % num2 == 0) {
+			return (true);
+		} else {
+			return (false);
+		}
+	}
+
+	// This method returns the absolute value of the number passed.
+	public static double absValue(double a) {
+		if (a >= 1) {
+			return (a);
+		} else {
+			return (a * -1.0);
+		}
+	}
+
+	// This method returns the larger of the values passed.
+	public static double max(double a, double b) {
+		if (a > b) {
+			return (a);
+		} else {
+			return (b);
+		}
+	}
+
+	// This method returns the larger of the values passed.
+	public static double max (double a, double b, double c) {
+		if(a > b && a > c){
+			return a;
+		}else if(b > a && b > c){
+			return b;
+		}else{
+			return c;
+		}
+	}
+	// This method returns the smaller of the values passed.
+	public static double min (double a, double b) {
+		if (a >b) {
+			return b;
+		}else { 
+			return a;	
+		}
+	}
+	// This method rounds a double correctly to 2 decimal places and returns a double.
+	public static double round2(double number){
+		double x=number*1000;
+		double y=x%10;
+		if (y >=5){
+			return (x-y+10)/1000;
+		}else{
+			return (x - y)/1000;
+		}
+
 	}
 }
-
-
